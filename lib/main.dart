916a81daedd156f1687f578904128e8c2c0e29b2
hkhudair <eng.hkhudair@gmail.com>
2023-07-29 09:45:52 +0300

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
